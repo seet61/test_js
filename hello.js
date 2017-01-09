86b7600a11cf8de5f39b3e91487147e9491f83a8
seet61 <seet61@gmail.com>
2017-01-09 19:52:33 +0300

    add calculator example

---
+++ hello.js
@@ -0,0 +1,16 @@
+/**
+ * Created by seet on 08.01.17.
+ */
+(function () {
+    //Глобальная переменная
+    var message = 'Привет';
+
+    //Функция для вывода переменной
+    function showMessage() {
+        alert(message);
+    }
+
+    //Вывод сообщения
+    showMessage();
+})();
+
